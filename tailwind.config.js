2774a92ef8ab9161717986ef8e24bfd1c7495dcf
ardhanurfan <ardhanurfan@gmail.com>
2023-06-10 10:38:36 +0700

    Init Project Vite, Tailwindcss, React, Typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        monserrat: ["Monserrat", "sans-serif"],
+      },
+      textColor: {
+        primaryText: "#015CBA",
+        seccondaryText: "#4B3979",
+      },
+      colors: {
+        primaryGreen: "#A7CD3A",
+        primaryYellow: "#FFC500",
+        primaryOrange: "#ED6010",
+        primaryBlue: "#005CBA",
+        seccondaryGreen: "#BBD578",
+        seccondaryYellow: "#F8DB25",
+        seccondaryOrange: "#F87837",
+        seccondaryBlue: "#4F9BFD",
+        error: "#BD1B1B",
+        link: "#060B81",
+        warning: "#FFC806",
+        success: "#1B8E27",
+      },
+    },
   },
   plugins: [],
 };
