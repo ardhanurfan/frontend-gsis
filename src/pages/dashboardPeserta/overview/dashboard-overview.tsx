9c7a510c0a313f6acfa9834ef1303967fc77e21c
Ibrahim Imam <ibrhmmm552@gmail.com>
2023-06-13 23:19:42 +0700

    feat: Dashboard Overview

---
+++ src/pages/dashboardPeserta/overview/dashboard-overview.tsx
@@ -0,0 +1,44 @@
+const Overview = () => {
+  return (
+    <>
+      <div className="w-full px-[70px]">
+        <h1 className="pt-[183px] header1 text-primaryText mb-[10px]">
+          Announcement
+        </h1>
+        <div className="card-announcement rounded-[10px] p-[1px] mb-80">
+          <div className="py-[40px] pl-[40px] pr-[98px] rounded-[10px] h-full w-full bg-white">
+            <h2 className="header2 text-primaryText">Title</h2>
+            <h3 className=" font-monserrat font-normal text-xs text-seccondaryBlue">
+              Created at XX/XX/XX HH:MM:SS
+            </h3>
+            <p className=" font-monserrat body-text mt-8 text-primaryText text-justify">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est
+              laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est
+              laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Overview;
---
+++ src/pages/dashboardPeserta/overview/empty-overview.tsx
@@ -0,0 +1,16 @@
+const EmptyOverview = () => {
+  return (
+    <>
+      <div className="w-full flex flex-col items-center justify-center h-screen">
+        <img
+          className="h-[200px] mb-8"
+          src="./src/assets/dashboard_overview_empty.svg"
+          alt=""
+        />
+        <p className="text-primaryText header1">There is nothing here...</p>
+      </div>
+    </>
+  );
+};
+
+export default EmptyOverview;
