3b2011a45c880e61348bb168ab4ada1027832a45
akmalnatha <akmalabhirama2002@gmail.com>
2023-06-14 20:05:49 +0700

    feat: edit profile peserta

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,21 @@ import Footer from "./components/footer/index";
 import Navbar from "./components/navbar/Navbar";
 import Homepage from "./pages/homepage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EditProfile from "./pages/dashboardPeserta/profilePeserta/edit";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="*" element={<FAQ />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Navbar />
+    //   <Routes>
+    //     <Route path="/" element={<Homepage />} />
+    //     <Route path="/faq" element={<FAQ />} />
+    //     <Route path="/about-us" element={<AboutUs />} />
+    //     <Route path="*" element={<FAQ />} />
+    //   </Routes>
+    //   <Footer />
+    // </BrowserRouter>
+    <EditProfile/>
   );
 }
 
