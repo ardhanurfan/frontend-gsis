77ce43ba0a8aa148850965fbaa85965ccb814288
Muhammad Hanif Al Faithoni <et4w4zz@gmail.com>
2023-06-16 19:58:38 +0700

    feat: Ceremony

---
+++ src/pages/ceremony/index.tsx
@@ -0,0 +1,18 @@
+import "./style.css";
+
+const Ceremony = () => {
+    return(
+        <>
+        <div className="w-full h-[3852px] bg-[url(./src/assets/Background_Ceremony.svg)] bg-cover">
+            <div className="pt-[150px] w-[64.5vw] mx-auto text-center">
+                <h1 className="w-auto mx-auto inline-block header1 text-primaryText">JUDUL WEBINAR</h1>
+                <div className="w-full h-auto bg-black">
+                    asda
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Ceremony;
