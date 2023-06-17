6d47325f2aa114065e46108f925dc1ca26f5299b
Muhammad Hanif Al Faithoni <et4w4zz@gmail.com>
2023-06-17 14:07:23 +0700

    feat: Ceremony

--- src/pages/ceremony/index.tsx
+++ src/pages/ceremony/index.tsx
@@ -7,7 +7,16 @@ const Ceremony = () => {
             <div className="pt-[150px] w-[64.5vw] mx-auto text-center">
                 <h1 className="w-auto mx-auto inline-block header1 text-primaryText">JUDUL WEBINAR</h1>
                 <div className="w-full h-auto bg-black">
-                    asda
+                    <div className="flex flex-row items-center gap-5 px-10 pt-10">
+                        <div className="bg-white w-full h-[484px] rounded-xl m-10">
+                            <div className="flex flex-row gap-1 m-5">
+                                <div className="h-[10px] w-[10px] rounded-full bg-primaryGreen"></div>
+                                <div className="h-[10px] w-[10px] rounded-full bg-primaryYellow"></div>
+                                <div className="h-[10px] w-[10px] rounded-full bg-primaryOrange"></div>
+                            </div>
+                        </div>
+                        <div className="bg-white w-full">1</div>
+                    </div>
                 </div>
             </div>
         </div>
