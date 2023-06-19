df19375adba9df43f2e2cfab1925acfb2de852f3
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2023-06-19 21:19:48 +0700

    add: competition dropdown

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,6 +3,11 @@ import "./Navbar.css";
 
 const Navbar = () => {
   const [navOpen, setNavOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <>
@@ -31,15 +36,56 @@ const Navbar = () => {
             >
               Ceremony
             </a>
+            <div className="">
+              <button
+                className="flex cursor-pointer hover:text-seccondaryBlue"
+                type="button"
+                onClick={toggleDropdown}
+              >
+                Competition
+                <span className="ml-2 w-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="h-5 w-5"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </span>
+              </button>
+              {isOpen && <ul
+                className="absolute z-[500] float-left mt-4 min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-xl shadow-primaryBlue"
+               
+              >
+                <li>
+                  <a
+                    className="block w-[200px] h-auto bg-transparent px-4 py-2 text-sm text-primaryBlue hover:bg-primaryBlue hover:text-seccondaryBlue"
+                    href="/coming-soon"
+          
+                  >
+                    Ganesha Social Impact Challenge 
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="block w-[200px] bg-transparent px-4 py-2 text-sm text-primaryBlue hover:bg-primaryBlue hover:text-seccondaryBlue"
+                    href="/bcc"
+                    
+                  >
+                    Bussiness Case Competition
+                  </a>
+                </li>
+                
+              </ul>}
+            </div>
             <a
               className="cursor-pointer hover:text-seccondaryBlue"
-              href="/coming-soon"
-            >
-              Competition
-            </a>
-            <a
-              className="cursor-pointer hover:text-seccondaryBlue"
-              href="/coming-soon"
+              href="/exhibition"
             >
               Exhibition
             </a>
@@ -125,7 +171,7 @@ const Navbar = () => {
                 </a>
                 <a
                   className="cursor-pointer hover:text-seccondaryBlue"
-                  href="/coming-soon"
+                  href="/exhibition"
                 >
                   Exhibition
                 </a>
