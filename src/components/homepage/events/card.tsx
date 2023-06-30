354d255cc61e71662fc831abed48f7e8ab45859f
Ardhan Nur Urfan <88726616+ardhanurfan@users.noreply.github.com>
2023-07-01 02:49:28 +0700

    fix: homepage

--- src/components/homepage/events/card.tsx
+++ src/components/homepage/events/card.tsx
@@ -1,24 +1,21 @@
-import './style.css'
+import "./style.css";
 
 interface EventCardProps {
-    image: string;
-    label: string;
+  image: string;
+  label: string;
 }
 
+const EventCard = ({ image, label }: EventCardProps) => {
+  return (
+    <div className="event-card w-[171px] h-[220px] xl:w-[350px] xl:h-[450px] overflow-hidden mb-[50px] cursor-pointer">
+      <div className=" h-[171px] xl:h-[350px] rounded-xl shadow-md shadow-slate-600">
+        <img src={image} alt="" className="p-4" />
+      </div>
+      <h2 className="font-montserrat text-white header3-mobile lg:header3 h-auto w-[171px] xl:w-[350px] mx-auto text-center mt-[20px]">
+        {label}
+      </h2>
+    </div>
+  );
+};
 
-const EventCard = ({
-    image,
-    label
-}: EventCardProps) => {
-    return (
-        <div className="event-card w-[171px] h-[220px] xl:w-[350px] xl:h-[450px] overflow-hidden mb-[50px] cursor-pointer">
-            <div className=" h-[171px] xl:h-[350px] rounded-xl shadow-md shadow-slate-600">
-            </div>  
-            <h2 className="font-montserrat text-white header3-mobile lg:header3 h-auto w-[171px] xl:w-[350px] mx-auto text-center mt-[20px]">
-                {label}
-            </h2>
-        </div>
-    )
-}
-
-export default EventCard
+export default EventCard;
--- src/components/homepage/intro/index.tsx
+++ src/components/homepage/intro/index.tsx
@@ -1,34 +1,49 @@
-import { Fragment } from "react"
+import { Fragment } from "react";
 
 const Intro = () => {
-    return (
-        <Fragment>
-            <div className="flex justify-between">
-                <div className="font-montserrat pt-[150px] lg:pt-[130px] w-[53%] lg:w-[47%] ml-[9%] ">
-                    <h1 className=" header1-mobile lg:header1 text-primaryBlue lg:pt-16">
-                        Ganesha Student Innovation Summit
-                    </h1>
-                    <p className="mt-[15px] text-justify text-primaryBlue body-text-mobile lg:body-text">
-                    Ganesha Student Innovation Summit (GSIS) adalah acara tahunan yang
-                    diselenggarakan oleh Keluarga Mahasiswa Institut Teknologi Bandung (KM ITB)
-                    untuk memfasilitasi siswa dan mahasiswa dari berbagai sekolah dan perguruan
-                    tinggi di Indonesia untuk mengembangkan ide kreatif dan inovatif dalam
-                    berbagai bidang, terkait teknologi, sains, sosial, seni dan budaya. Acara ini
-                    bertujuan untuk mendorong para peserta untuk mengembangkan kemampuan
-                    inovasi dan kewirausahaan mereka serta memperluas jaringan profesional
-                    mereka. Acara ini meliputi serangkaian kegiatan seperti kompetisi inovasi dan
-                    business case, pameran, dan konferensi internasional yang diikuti oleh para
-                    peserta dari berbagai perguruan tinggi di Indonesia dan negara-negara
-                    tetangga
-                    </p>
-                </div>
-                <div className="pt-[150px] sm:pt-[140px] lg:pt-[130px] w-2/6 2xl:w-fit ">
-                    <img src="./src/assets/title_ilustration.svg" alt="" className="relative"/>
-                </div>
-            </div>
-            <img src="./src/assets/theme_elaboration.svg" alt="" className="pt-[62px] relative px-4 lg:mx-auto"/>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <div className="flex justify-center lg:justify-between items-center">
+        <div className="font-montserrat pt-[150px] lg:pt-[130px] w-[80%] lg:w-[47%] lg:ml-24">
+          <h1 className="header3 lg:header1 text-primaryBlue lg:pt-16 text-center">
+            Ganesha Student Innovation Summit
+          </h1>
+          <p className="mt-[15px] text-justify text-primaryBlue body-text-mobile rounded-xl p-6 bg-slate-100 shadow-lg backdrop-blur-sm shadow-slate-500 bg-opacity-30 lg:body-text lg:shadow-none lg:bg-transparent">
+            Ganesha Student Innovation Summit (GSIS) adalah acara tahunan yang
+            diselenggarakan oleh Keluarga Mahasiswa Institut Teknologi Bandung
+            (KM ITB) untuk memfasilitasi siswa dan mahasiswa dari berbagai
+            sekolah dan perguruan tinggi di Indonesia untuk mengembangkan ide
+            kreatif dan inovatif dalam berbagai bidang, terkait teknologi,
+            sains, sosial, seni dan budaya. Acara ini bertujuan untuk mendorong
+            para peserta untuk mengembangkan kemampuan inovasi dan kewirausahaan
+            mereka serta memperluas jaringan profesional mereka. Acara ini
+            meliputi serangkaian kegiatan seperti kompetisi inovasi dan business
+            case, pameran, dan konferensi internasional yang diikuti oleh para
+            peserta dari berbagai perguruan tinggi di Indonesia dan
+            negara-negara tetangga
+          </p>
+        </div>
+        <div className="hidden lg:block pt-[150px] sm:pt-[140px] lg:pt-[130px] w-2/6 2xl:w-fit ">
+          <img
+            src="./src/assets/title_ilustration.svg"
+            alt=""
+            className="relative"
+          />
+        </div>
+      </div>
+      <h1 className="text-center header3 lg:header1 text-primaryBlue mt-20">
+        GSIS Theme Elaboration
+      </h1>
+      <h1 className="text-center header3-mobile lg:header2 text-primaryBlue py-6 lg:py-12">
+        'Futurizing' Sustainability : <br /> Collaborative Revolution
+      </h1>
+      <img
+        src="./src/assets/theme.svg"
+        alt=""
+        className="relative px-4 lg:mx-auto"
+      />
+    </Fragment>
+  );
+};
 
-export default Intro
+export default Intro;
