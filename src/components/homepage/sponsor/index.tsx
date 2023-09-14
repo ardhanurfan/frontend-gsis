a6e5b39689e5f1ec954d2f6d02f142a800a056f6
Ardhan Nur Urfan <88726616+ardhanurfan@users.noreply.github.com>
2023-09-14 22:30:33 +0700

    fix: sponsor, fix: timeline

--- src/components/homepage/sponsor/index.tsx
+++ src/components/homepage/sponsor/index.tsx
@@ -4,16 +4,31 @@ const Sponsored = () => {
       <h1 className="flex header1-mobile lg:header1 justify-center pb-[25px] lg:pb-[50px] text-primaryBlue">
         Sponsored By
       </h1>
-      <div className="brand-container w-[86%] h-auto bg-slate-100 mx-auto rounded-xl flex flex-col lg:flex-row justify-center p-10 bg-opacity-30">
+      <div className="brand-container w-[86%] h-auto bg-slate-100 mx-auto rounded-xl flex flex-wrap flex-col lg:flex-row justify-center items-center p-10 bg-opacity-30 gap-8">
         <img
           src="sponsor/cakap.png"
           alt=""
-          className="lg:h-36 h-20 object-contain"
+          className="lg:w-[450px] w-80 object-contain"
         />
         <img
           src="sponsor/ef.png"
           alt=""
-          className="lg:h-36 h-20 object-contain"
+          className="lg:w-[450px] w-80 object-contain"
+        />
+        <img
+          src="sponsor/smartfren.png"
+          alt=""
+          className="lg:w-[450px] w-80 object-contain"
+        />
+        <img
+          src="sponsor/sampoerna.png"
+          alt=""
+          className="lg:w-[450px] w-80 object-contain"
+        />
+        <img
+          src="sponsor/pertamina.png"
+          alt=""
+          className="lg:w-[600px] w-80 object-contain"
         />
       </div>
     </div>
